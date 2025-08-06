7343f47f02dac987f0c38d71eb05248aa433d73d
Chinmay Thete <michinmaythete@gmail.com>
2025-08-06 12:08:58 +0530

    v2

--- download_model.sh
+++ download_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author:Chinmay Thete
 # Date Created:05/08/2025
-# Modification Date:05/08/2025
+# Modification Date:06/08/2025
 # Description: Download pickle file of model.
 # Usage:bash download_model.sh
 # prerequiest:Files should be on Google Drive.
@@ -10,13 +10,13 @@
 echo "📥 Downloading model files from Google Drive..."
 
 # Replace these with your actual Google Drive file IDs
-RF_ID="your-random-forest-id"
-SCALER_ID="your-scaler-id"
-COLS_ID="your-feature-columns-id"
+RF_ID="1udq4rkuf1ZEcOpPidSo5CTXPAM10Klqx"
+SCALER_ID="1Eq595Pnp8L0-106dz5UyzMq0EwKSGQZV"
+
 
 # Download each file using curl
 curl -L -o random_forest.pkl "https://drive.google.com/uc?export=download&id=${RF_ID}"
 curl -L -o scaler.pkl "https://drive.google.com/uc?export=download&id=${SCALER_ID}"
-curl -L -o feature_columns.pkl "https://drive.google.com/uc?export=download&id=${COLS_ID}"
 
-echo "✅ All model files downloaded successfully!"
+
+echo "All model files downloaded successfully!"
